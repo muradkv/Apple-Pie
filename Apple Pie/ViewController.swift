77d39bef2cff90feb702c7aba66daa113f4693e0
murad <muradkv@gmail.com>
2019-07-30 06:28:35 +0300

    Add IBOutlets and IBAction

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false 
+    }
+    
 }
 
