94fa059e324bc2d426fb260ec212bb109804dc76
murad <muradkv@gmail.com>
2019-08-04 23:52:47 +0300

    Handle win or lose game, add updateGameState func

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController {
     var listOfWords = ["buccaner", "swift", "glorious", "incandescent", "bug", "program"]
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     
     var currentGame: Game!
     
@@ -47,12 +55,23 @@ class ViewController: UIViewController {
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
 
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
         print(letterString)
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
+        updateGameState()
     }
     
 }
